da70e273e007cc1caf499fbedd3ea3dfd6e6f807
James Byrnes <noobularkid@gmail.com>
2018-10-28 18:02:55 -0400

    updated roster
    Mentor / Students / Events updated!

--- sites/bioniczebras/data/mentors.js
+++ sites/bioniczebras/data/mentors.js
@@ -3,14 +3,10 @@ var mentors = [
     {"name":"Karl Heinrich"}, 
     {"name":"Donnie Castaldini"},
     {"name":"Scott Simons"},
-    {"name":"Josh Johnson"},
-    {"name":"Beth Tolliver"},
-    {"name":"Kelly Barnett"}, 
-    {"name":"Trevor Davis"},        
-    {"name":"Nick Curry"},        
-    {"name":"Kayla Vuich"},       
-    {"name":"Mason Webb"},       
-    {"name":"Shana Totten"},         
-    {"name":"Naaman Chaudray"},       
-    {"name":"Chrstian Jones"}
+    {"name":"Mykala Smith"},  
+    {"name":"Josh Johnson"},  
+    {"name":"Eric Cleereman"},  
+    {"name":"Mitch Castaldini"},    
+    {"name":"Naaman Chaudry"}
+                  
 ]
