c2dfdde968a966f98c4337f42462713b7d5aedda
James Byrnes <noobularkid@gmail.com>
2018-11-02 01:49:46 -0400

    Update 1.03
    -Added the rest of the pages.
    -contacts,resources,meetings finished
    -sponsors,media,annoucements WIP

---
+++ sites/bioniczebras/data/announcements.js
@@ -0,0 +1,17 @@
+// Template
+/* 
+
+{
+    "annoucement" : "",
+    "writedate" : ""
+},
+
+REMOVE COMMA ON THE LAST INDEX!
+*/
+
+var annoucements = [
+    {
+        "annoucement" : "For those who haven't already, the payments for Additional Regional Event(s), 2016 FIRST Championship (Pre-Qualified FRC Teams), and Additional In-District Event(s) are due on 1/29/2016",
+        "writedate" : "1/18/2018"
+    }
+]
