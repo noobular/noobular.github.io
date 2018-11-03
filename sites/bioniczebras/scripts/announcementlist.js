07e82c50d3f4e2896478ca92a1f71c2294cb9d17
James Byrnes <noobularkid@gmail.com>
2018-11-03 15:28:56 -0400

    Update announcementlist.js

--- sites/bioniczebras/scripts/announcementlist.js
+++ sites/bioniczebras/scripts/announcementlist.js
@@ -5,7 +5,7 @@ function listAnnouncements(){
         var element = document.createElement("li");
         $(element).addClass("nav-item");
         element.style = "margin-left:10px;"
-        element.innerHTML = announcements[index].announcement;
+        element.innerHTML = "test"//announcements[index].announcement;
         $("#announcementlist").append(element);
     })
 }
