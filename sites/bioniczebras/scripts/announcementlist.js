93d9cbf4e5c38f7d3bb116b42948c18e74a5eeb9
James Byrnes <noobularkid@gmail.com>
2018-11-03 15:07:08 -0400

    Update announcementlist.js

--- sites/bioniczebras/scripts/announcementlist.js
+++ sites/bioniczebras/scripts/announcementlist.js
@@ -1,1 +1,17 @@
-x
+
+function listAnnouncements(){
+    // Run through every index of the mentors json, and do this function for every listed index
+    $(announcements).each(function(index){
+        var element = document.createElement("li");
+        $(element).addClass("nav-item");
+        element.style = "margin-left:10px;"
+        element.innerHTML = announcements[index].name;
+        $("#announcementlist").append(element);
+    })
+}
+
+
+
+
+// Run order
+listAnnouncements();
