4235a05d3f5a065d923c62f77a2e6475bea71b6e
James Byrnes <noobularkid@gmail.com>
2018-11-03 15:35:38 -0400

    Update announcements.js

--- sites/bioniczebras/data/announcements.js
+++ sites/bioniczebras/data/announcements.js
@@ -12,7 +12,12 @@ REMOVE COMMA ON THE LAST INDEX!
 
 var announcements = [
     {
+        "announcement":"",
+        "writedate":""
+    },
+    {
         "announcement":"No current announcements",
         "writedate":"11/4/18"
     }
+    
 ]
