f982ce5dfe34f428a08be807f86413f945c321c5
James Byrnes <noobularkid@gmail.com>
2018-11-24 20:03:53 -0500

    added

--- sites/bioniczebras/data/showcase.js
+++ sites/bioniczebras/data/showcase.js
@@ -4,6 +4,13 @@
 // Remove comma if its the last index!!!
 
 var images = [
-    {"name":"buildseason2018.jpg","header":"Build Season 2018","caption":"We had a great time building this year!"}
+    {"name":"buildseason2018.jpg","header":"Build Season 2018","caption":"We had a great time building this year!"},
+    {"name":"1.jpg","header":"","caption":""},
+    {"name":"2.jpg","header":"","caption":""},
+    {"name":"3.jpg","header":"","caption":""},
+    {"name":"4.jpg","header":"","caption":""},
+    {"name":"5.jpg","header":"","caption":""},
+    {"name":"6.jpg","header":"","caption":""},
+    {"name":"7.jpg","header":"","caption":""}
     //Remove comma on the last index!
 ]
