6f0ab3d1a408f62b6ecad8c59ffd83042096bc77
Noobular <noobularkid@gmail.com>
2020-02-02 01:05:20 -0500

    ReactJS Build v1.0.0
    Finally a ReactJS Build .. if it works first try.

---
+++ precache-manifest.1b4394c91563772aefdaa871dec454de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e830bd8886944d26f93622ff160278",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af715a1ff9ddc676eb19",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9e8d1476256f341fa01a",
+    "url": "/static/css/main.78f7cfe4.chunk.css"
+  },
+  {
+    "revision": "af715a1ff9ddc676eb19",
+    "url": "/static/js/2.ac2b3e1c.chunk.js"
+  },
+  {
+    "revision": "9682237fa99bc1a358b1ac2de59fd99c",
+    "url": "/static/js/2.ac2b3e1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e8d1476256f341fa01a",
+    "url": "/static/js/main.6c272d28.chunk.js"
+  },
+  {
+    "revision": "f86c4c110c0adcac7846",
+    "url": "/static/js/runtime-main.fe661426.js"
+  },
+  {
+    "revision": "a74a3e14b0aabafee08c486150e9114c",
+    "url": "/static/media/logo.a74a3e14.png"
+  },
+  {
+    "revision": "9ef0ad8c37563d35fa7c04f1a5a2da83",
+    "url": "/static/media/parallax.9ef0ad8c.png"
+  }
+]);
