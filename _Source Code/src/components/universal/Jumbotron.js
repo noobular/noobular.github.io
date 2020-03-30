181d314103fd3a4c5b6d4cf4d6b3d534d7123693
Noobular <noobularkid@gmail.com>
2020-03-30 10:24:32 -0400

    ReactJS Update 1.0.9
    Changed jumbotron text color scheme to all white.

--- _Source Code/src/components/universal/Jumbotron.js
+++ _Source Code/src/components/universal/Jumbotron.js
@@ -17,18 +17,20 @@ export default function Jumbotron() {
                             <div id="jumbotron-content" style={{color:"white"}}>
                                 <h1 className="d-flex justify-content-center jumbo_name">James Byrnes</h1>
                                 <p  className="d-flex justify-content-center jumbo_description">Software Developer - NodeJS / ReactJS Developer</p>
+
                                 <p  className="d-flex justify-content-center">
-                                    <a href="mailto:JamesEdwardByrnes@gmail.com"         target="_top"    rel="noopener noreferrer" className="jumbotron-text  btn btn-outline-danger" > <i className="far fa-envelope"/> Email</a>
-                                    <a href="https://www.github.com/noobular"            target="_BLANK"  rel="noopener noreferrer" className="jumbotron-text  btn btn-outline-danger btn-lg"><i className="fab fa-github"/> Github</a>
-                                    <a href="https://www.Linkedin.com/in/jamesebyrnes/"  target="_BLANK"  rel="noopener noreferrer" className="jumbotron-text btn btn-outline-danger btn-lg"><i className="fab fa-linkedin"/> LinkedIn</a>   
+                                    <a href="mailto:JamesEdwardByrnes@gmail.com"         target="_top"    rel="noopener noreferrer" className="jumbotron-text  btn btn-outline-light" > <i className="icon far fa-envelope"/> Email</a>
+                                    <a href="https://www.github.com/noobular"            target="_BLANK"  rel="noopener noreferrer" className="jumbotron-text  btn btn-outline-light btn-lg"><i className="icon fab fa-github"/> Github</a>
+                                    <a href="https://www.Linkedin.com/in/jamesebyrnes/"  target="_BLANK"  rel="noopener noreferrer" className="jumbotron-text btn btn-outline-light btn-lg"><i className="icon fab fa-linkedin"/> LinkedIn</a>   
                                 </p>
+
                                 <hr className="my-1"/>
-                                
+
 
                                 <h5 className="d-flex justify-content-center" style={{fontSize:36,marginTop:50}}>Resume</h5>
                                 <div className="d-flex justify-content-center">
-                                    <a href="/pdf/Resume.pdf" target="_BLANK"  rel="noopener noreferrer" className="btn btn-outline-danger btn-lg jumbotron-text" style={{margin:10}}><i className="far fa-file-pdf"></i> PDF</a>
-                                    <a href="https://docs.google.com/document/d/1l69iJdJAG4k8u19XkRy_A5ZbhpSjRuyZCcazsLGCm8g/edit?usp=sharing" target="_BLANK"  rel="noopener noreferrer" className="btn btn-outline-danger btn-lg jumbotron-text" style={{margin:10}}><i className="fab fa-google-drive"></i> Google Doc</a>
+                                    <a href="/pdf/Resume.pdf" target="_BLANK"  rel="noopener noreferrer" className="btn btn-outline-light btn-lg jumbotron-text" style={{margin:10}}><i className="icon far fa-file-pdf"></i> PDF</a>
+                                    <a href="https://docs.google.com/document/d/1l69iJdJAG4k8u19XkRy_A5ZbhpSjRuyZCcazsLGCm8g/edit?usp=sharing" target="_BLANK"  rel="noopener noreferrer" className="btn btn-outline-light btn-lg jumbotron-text" style={{margin:10}}><i className="icon fab fa-google-drive"></i> Google Doc</a>
                                 </div>
                             </div>
                         </div>
