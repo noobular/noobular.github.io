1e39df9e9301824ddda4a373a3e047c21f9a2458
James Byrnes <noobularkid@gmail.com>
2022-02-10 06:37:17 -0500

    Updates

---
+++ precache-manifest.371c46f3bbc6a6c4b842adfb3969ee70.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9e2bebfa84e0a5525fa1f472eca8ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b5b736179478e3cd989",
+    "url": "/static/css/2.509c4ec2.chunk.css"
+  },
+  {
+    "revision": "1fe0036e5720b1224a5c",
+    "url": "/static/css/main.1b36dd79.chunk.css"
+  },
+  {
+    "revision": "2b5b736179478e3cd989",
+    "url": "/static/js/2.f31dbd05.chunk.js"
+  },
+  {
+    "revision": "b4f9955a46235ab5d9673fafd3d3f6c5",
+    "url": "/static/js/2.f31dbd05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fe0036e5720b1224a5c",
+    "url": "/static/js/main.9f7f5c9c.chunk.js"
+  },
+  {
+    "revision": "f86c4c110c0adcac7846",
+    "url": "/static/js/runtime-main.fe661426.js"
+  },
+  {
+    "revision": "a74a3e14b0aabafee08c486150e9114c",
+    "url": "/static/media/logo.a74a3e14.png"
+  },
+  {
+    "revision": "9ef0ad8c37563d35fa7c04f1a5a2da83",
+    "url": "/static/media/parallax.9ef0ad8c.png"
+  }
+]);
