eed7bc2b82ea089361f746782062c7744b0e88e7
James Byrnes <noobularkid@gmail.com>
2022-02-10 06:04:50 -0500

    Updates

--- precache-manifest.371c46f3bbc6a6c4b842adfb3969ee70.js
+++ precache-manifest.371c46f3bbc6a6c4b842adfb3969ee70.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8ad62bf40c92e6e9258f0adc0935706d",
+    "revision": "0e9e2bebfa84e0a5525fa1f472eca8ab",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.509c4ec2.chunk.css"
   },
   {
-    "revision": "802d0249589f3bc6e517",
+    "revision": "1fe0036e5720b1224a5c",
     "url": "/static/css/main.1b36dd79.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f31dbd05.chunk.js.LICENSE"
   },
   {
-    "revision": "802d0249589f3bc6e517",
-    "url": "/static/js/main.ee7ccaac.chunk.js"
+    "revision": "1fe0036e5720b1224a5c",
+    "url": "/static/js/main.9f7f5c9c.chunk.js"
   },
   {
     "revision": "f86c4c110c0adcac7846",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43e14ddeb905a0de9e301eccad1675e5.js"
+  "/precache-manifest.371c46f3bbc6a6c4b842adfb3969ee70.js"
 );
 
 self.addEventListener('message', (event) => {
